ea3243174be5bc82b86dadf63ba72a56f7f9a4fe
RTMoney <RTMoney@users.noreply.github.com>
2019-02-04 22:36:57 -0600

    port and directory updates

--- src/MempoolStatus.cpp
+++ src/MempoolStatus.cpp
@@ -341,7 +341,7 @@ MempoolStatus::is_thread_running()
     return is_running;
 }
 
-bf::path MempoolStatus::blockchain_path {"/home/mwo/.solo/lmdb"};
+bf::path MempoolStatus::blockchain_path {"/home/ubuntu/.solo/lmdb"};
 string MempoolStatus::deamon_url {"http:://127.0.0.1:22423"};
 cryptonote::network_type MempoolStatus::nettype {cryptonote::network_type::MAINNET};
 atomic<bool>       MempoolStatus::is_running {false};
